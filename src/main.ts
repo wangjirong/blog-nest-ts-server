d65976d3b798805783e78df90b93bac5cc5e3409
wangjirong <408718358@qq.com>
2020-06-29 20:38:43 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  await app.listen(9000);
 }
 bootstrap();
